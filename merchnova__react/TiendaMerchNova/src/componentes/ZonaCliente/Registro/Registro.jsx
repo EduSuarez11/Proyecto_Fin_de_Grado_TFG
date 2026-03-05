aec22b2742091e03c599666b9305c7295933c40d
EduSuarez11 <eduardosuarezalvarado3@gmail.com>
2026-03-06 00:37:39 +0100

    Cambios de registro

--- merchnova__react/TiendaMerchNova/src/componentes/ZonaCliente/Registro/Registro.jsx
+++ merchnova__react/TiendaMerchNova/src/componentes/ZonaCliente/Registro/Registro.jsx
@@ -13,36 +13,41 @@ function Registro() {
     }
 
     async function handleSubmit(ev) {
-        ev.preventDefault();
-        console.log('Datos form: ', formRegistro)
+        try {
+            ev.preventDefault();
+            console.log('Datos form: ', formRegistro)
 
-        const response = await fetch('api/Cliente/Registro', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formRegistro)
-        });
+            const response = await fetch('http://localhost:3000/api/Cliente/Registro', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formRegistro)
+            });
 
-        const data = await response.json();
+            const data = await response.json();
+            console.log('Respuesta del servidor: ', data.message);
+        } catch (error) {
+            console.log('Error en el Registro: ', error);
+        }
     }
 
     return (
         <div className="register-wrapper">
             <div className="register-card">
                 <h2 className="text-center register-title">Creando tu Cuenta</h2>
-                <form method="POST" onSubmit={handleSubmit}>
+                <form method="POST">
                     {
                         ['nombre', 'email', 'password', 'confirmPassword'].map((value, pos) => (
                             <InputHTMLComponent
-                                 key={pos}
-                                 id={value}
-                                 labelInput={value === 'confirmPassword' ? 'Confirmar Contraseña' : value.charAt(0).toUpperCase() + value.slice(1)}
-                                 nameInput={value}
-                                 tipo={value === 'email' ? 'email' : (value === 'password' ? 'password' : 'text')}
-                                 placeholder={value === 'confirmPassword' ? 'Repite tu contraseña' : `Escribe tu ${value}`}
-                                 change={onChangeInput}
-                             />
+                                key={pos}
+                                id={value}
+                                labelInput={value === 'confirmPassword' ? 'Confirmar Contraseña' : value.charAt(0).toUpperCase() + value.slice(1)}
+                                nameInput={value}
+                                tipo={value === 'email' ? 'email' : (value === 'password' ? 'password' : 'text')}
+                                placeholder={value === 'confirmPassword' ? 'Repite tu contraseña' : `Escribe tu ${value}`}
+                                change={onChangeInput}
+                            />
 
                         ))
                     }
@@ -57,7 +62,7 @@ function Registro() {
                         </select>
                     </div>
                     <div className="d-grid">
-                        <button type="submit" className="btn btn-custom">Registrarse</button>
+                        <button type="submit" className="btn btn-custom" onClick={handleSubmit}>Registrarse</button>
                     </div>
                 </form>
             </div>
