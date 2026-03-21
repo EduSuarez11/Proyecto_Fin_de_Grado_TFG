f20ff4f4a5925b73354a59c29b9787035aaa4649
EduSuarez11 <eduardosuarezalvarado3@gmail.com>
2026-03-21 18:15:45 +0100

    Cambio en endpoint categoria slug

--- merchnova__nodejs/configuration_pipelines/configuration_endpoints/endPointsTienda.js
+++ merchnova__nodejs/configuration_pipelines/configuration_endpoints/endPointsTienda.js
@@ -17,20 +17,27 @@ shopRouter.get('/Productos', async (req, res, next) => {
     } finally {
         await mongoose.connection.close();
     }
-    
+
 });
 
-shopRouter.get('/Producto/Camiseta/:slug', async (req, res, next) => {
+shopRouter.get('/Producto/:categoria/:slug', async (req, res, next) => {
     try {
         await mongoose.connect(process.env.URL_MONGODB);
         const getProduct = await mongoose.connection.collection('productos').findOne({
+            categoria: req.params.categoria,
             slug: req.params.slug
         })
 
         if (!getProduct) throw new Error('Producto no encontrado en la base de datos');
 
-        console.log('Producto encontrado: ', getProduct);
-        res.status(200).send({code: 0, message: 'Producto obtenido', product: getProduct});
+        const moreProducts = await mongoose.connection.collection('productos').aggregate([{
+            $match: {
+                nombre: { $ne: getProduct.nombre }
+            }
+        }, { $sample: { size: 4 } }]).toArray();
+
+        //console.log('Producto encontrado: ', getProduct);
+        res.status(200).send({ code: 0, message: 'Producto obtenido', product: getProduct, moreProducts });
     } catch (error) {
         console.log('Error al obtener el producto: ', error);
         res.status(200).send({ code: 2, message: `Error al obtener el producto: ${error}` });
@@ -42,12 +49,12 @@ shopRouter.get('/Producto/Camiseta/:slug', async (req, res, next) => {
 shopRouter.get('/Productos/Home', async (req, res, next) => {
     try {
         await mongoose.connect(process.env.URL_MONGODB);
-        const products = await mongoose.connection.collection('productos').find().sort({precio: -1}).limit(4).toArray();
+        const products = await mongoose.connection.collection('productos').find().sort({ precio: -1 }).limit(4).toArray();
 
         if (!products) throw new Error('Productos no encontrados en la base de datos');
 
         console.log('Producto encontrado: ', products);
-        res.status(200).send({code: 0, message: 'Producto home obtenidos', data: products});
+        res.status(200).send({ code: 0, message: 'Producto home obtenidos', data: products });
     } catch (error) {
         console.log('Error al obtener el producto: ', error);
         res.status(200).send({ code: 3, message: `Error al obtener el producto: ${error}` });
