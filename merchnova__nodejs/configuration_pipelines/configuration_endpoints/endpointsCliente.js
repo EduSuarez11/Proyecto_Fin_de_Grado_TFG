2b3986dfcca507781a8c6615f7f6fa6a95da4e8e
EduSuarez11 <eduardosuarezalvarado3@gmail.com>
2026-04-21 00:49:13 +0200

    Funciones para cambiar la password

--- merchnova__nodejs/configuration_pipelines/configuration_endpoints/endpointsCliente.js
+++ merchnova__nodejs/configuration_pipelines/configuration_endpoints/endpointsCliente.js
@@ -94,10 +94,10 @@ clientRouter.get('/ActivacionCuenta', async (req, resp, next) => {
         if (!updateData) throw new Error('No se pudo activar la cuenta.');
 
         //console.log('Cuenta activada correctamente');
-        resp.redirect(`${process.env.URL_FRONTEND_LOGIN}Cliente/Login?activada=true`);
+        resp.redirect(`${process.env.URL_FRONTEND}Cliente/Login?activada=true`);
     } catch (error) {
         //console.log('Error en la activacion de cuenta: ', error);
-        resp.redirect(`${process.env.URL_FRONTEND_LOGIN}Cliente/Login?activada=false`);
+        resp.redirect(`${process.env.URL_FRONTEND}Cliente/Login?activada=false`);
     }
 })
 
@@ -269,7 +269,7 @@ clientRouter.get('/Verify/Token', async (req, res, next) => {
 
         res.status(200).send({ code: 0, message: 'Token verificado.', data: { user } });
     } catch (error) {
-        res.status(200).send({ code: 8, message: `${error}` });
+        res.status(200).send({ code: 8, message: error.message });
     }
 });
 
@@ -309,6 +309,45 @@ clientRouter.post('/Perfil/Update', async (req, res, next) => {
     }
 });
 
+
+clientRouter.post('/ForgotPassword', async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        console.log('Email recibido para recuperar contraseña: ', email);
+        const existClient = await mongoose.connection.collection('clientes').findOne({ 'cuenta.email': email });
+
+        if (existClient === null) throw new Error('Ese correo no se encuentra registrado.');
+
+        mailjetService.sendEmailForSetPassword(existClient, email);
+
+        res.status(200).send({ code: 0, message: 'Recibirás un email para restablecer tu contraseña.' })
+    } catch (error) {
+        res.status(200).send({ code: 12, message: error.message })
+    }
+});
+
+
+clientRouter.post('/ResetPassword', async (req, res, next) => {
+    try {
+        const { password, confirmPassword } = req.body;
+        console.log('Datos del req.body: ', req.body);
+        //        const existClient = await mongoose.connection.collection('clientes').findOne({ _id: new mongoose.Types.ObjectId(clientId) });
+
+        if (confirmPassword !== password) throw new Error('Las contraseñas deben coincidir');
+        if (confirmPassword.length < 6) throw new Error('La contraseña debe tener al menos 6 caracteres.');
+        if (confirmPassword.length > 25) throw new Error('La contraseña no puede tener más de 25 caracteres.');
+
+        const updatePassword = await mongoose.connection.collection('clientes').updateOne(
+            { _id: new mongoose.Types.ObjectId(req.body.clientId) },
+            { $set: { 'cuenta.password': bcrypt.hashSync(password, 10) } }
+        )
+        if (updatePassword.modifiedCount === 0) throw new Error('No se pudo actualizar la contraseña.');
+        console.log('Contraseña cambiada: ', updatePassword);
+        res.status(200).send({ code: 0, message: 'Contraseña actualizada con éxito.' })
+    } catch (error) {
+        res.status(200).send({ code: 13, message: error.message })
+    }
+});
 
 clientRouter.post('/NewDirection', async (req, res, next) => {
     try {
