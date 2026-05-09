a789ae611541f890b0202bbfbb30fb2f21201f17
EduSuarez11 <eduardosuarezalvarado3@gmail.com>
2026-05-10 00:36:50 +0200

    Comunicacion entre clientes y servidor ok

--- merchnova__nodejs/configuration_socket_io_server/socket_io_server.js
+++ merchnova__nodejs/configuration_socket_io_server/socket_io_server.js
@@ -30,22 +30,39 @@ module.exports = (serverNode) => {
                 const admins = await getAdmins();
 
                 console.log('Administradores disponibles para asignar al cliente');
-                // Asignar un administrador aleatorio
-                const assignedAdminId = admins[Math.floor(Math.random() * admins.length)]._id.toString();
 
-                memoryStorage.set(keyChat, { clientId, adminId: assignedAdminId });
+                // Asignar un administrador aleatorio
+                const assignedAdmin = admins[Math.floor(Math.random() * admins.length)];
+                const assignedAdminId = assignedAdmin._id.toString();
+
+                const adminData = {
+                    id: assignedAdminId,
+                    nombre: assignedAdmin.nombreCompleto,
+                    imagenCuenta: assignedAdmin.cuenta.imagenCuenta
+                }
+
+                socket.emit('assignedAdmin', JSON.stringify(adminData));
+                memoryStorage.set(keyChat, { clientId, adminId: assignedAdminId, messages: [], adminData });
 
                 const defaultMsg = {
                     contenido: 'Bienvenido al soporte técnico, ¿necesitas ayuda?. Aquí podrás resolver tus dudas.',
                     transmitterId: assignedAdminId,
                     timestamp: new Date().toISOString().slice(0, 19).replace('T', ' '),
                 }
-                //io.to(keyChat).emit('receiveMsg', defaultMsg);
+                // Realizar el emit al cliente para que reciba el mensaje de bienvenida
+                io.to(keyChat).emit('receiveMsg', JSON.stringify(defaultMsg));
+            } else {
+                const session = memoryStorage.get(keyChat);
+                //console.log('Datos de la sesión encontrada para el cliente: ', session);
 
-                // socket.emit(`notification_admin_${assignedAdminId}`, {
-                //     keyChat,
-                //     clientId
-                // });
+                const adminData = {
+                    id: session.adminId,
+                    nombre: session.adminData.nombre,
+                    imagenCuenta: session.adminData.imagenCuenta
+                }
+
+                socket.emit('assignedAdmin', JSON.stringify(adminData));
+                memoryStorage.set(keyChat, { clientId, adminId: session.adminId, messages: [], adminData });
 
             }
         });
@@ -53,18 +70,39 @@ module.exports = (serverNode) => {
         // Enviar mensaje con la sala ya creada
         socket.on('sendMsg', (data) => {
             const { keyChat, contenido, transmitterId, timestamp } = data;
-            io.to(keyChat).emit('receiveMsg', data);
-            console.log(`Mensaje para la sala ${keyChat}: ${contenido}`);
+            const session = memoryStorage.get(keyChat);
+
+            console.log(`NUEVO MENSAJE de [${transmitterId}] para la sala [${keyChat}]: ${contenido}`);
+            io.to(keyChat).emit('receiveMsg', JSON.stringify(data));
+            //socket.emit('adminJoinRoom', keyChat);
+            if (session) {
+                const adminId = session.adminId;
+                session.messages.push({ contenido, transmitterId, timestamp });
+                console.log(`Avisando al admin ${adminId} que hay un mensaje nuevo en ${keyChat}`);
+
+                io.emit(`notification_admin_${adminId}`, {
+                    keyChat: keyChat,
+                    ultimoMensaje: contenido,
+                    clientId: session.clientId
+                });
+            }
+            // socket.emit(`notification_admin_${session.adminId}`, {
+            //     keyChat,
+            //     transmitterId
+            // });
+
         });
 
         // El administrador se une a la sala del cliente
-        socket.on('adminJoinRoom', (keyChat, adminId) => {
+        socket.on('adminJoinRoom', (keyChat) => {
             socket.join(keyChat);
             console.log('Administrador se ha unido a la sala de soporte: ', keyChat);
-            socket.emit(`notification_admin_${adminId}`, {
-                keyChat,
-                clientId: memoryStorage.get(keyChat)?.clientId
-            });
+
+            const session = memoryStorage.get(keyChat);
+            if (session && session.messages.length > 0 && session.messages.length <= 1) {
+                console.log('Enviando historial de mensajes al administrador para la sala: ', keyChat);
+                socket.emit('historyChat', JSON.stringify(session.messages));
+            }
         })
     })
 }
