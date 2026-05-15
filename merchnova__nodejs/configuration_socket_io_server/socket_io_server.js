5f65077882274aba87171a51ce10dc1484fde661
EduSuarez11 <eduardosuarezalvarado3@gmail.com>
2026-05-16 00:50:13 +0200

    Cambios en el chat

--- merchnova__nodejs/configuration_socket_io_server/socket_io_server.js
+++ merchnova__nodejs/configuration_socket_io_server/socket_io_server.js
@@ -1,16 +1,12 @@
 const { Server } = require("socket.io");
 const mongoose = require('mongoose');
 
+
 const memoryStorage = new Map();
 
-async function getAdmins() {
-    const admins = await mongoose.connection.collection('clientes').find({ 'cuenta.rol': 'ADMINISTRADOR' }).toArray();
-    return admins;
-}
+async function createChatAdmin(data) {
+    // Realizar peticion al backend para actualizar el cliente admin y crear el chat en el admin en la base de datos
 
-async function getClientById(clientId) {
-    const client = await mongoose.connection.collection('clientes').findOne({ _id: new mongoose.Types.ObjectId(clientId) });
-    return client;
 }
 
 module.exports = (serverNode) => {
@@ -31,15 +27,12 @@ module.exports = (serverNode) => {
 
             const session = memoryStorage.get(salaId);
             //console.log('Mensajes: ', mensaje);
+
             console.log(`NUEVO MENSAJE de [${mensaje.transmitterId}] para la [${salaId}]: ${mensaje.contenido} mandado en ${mensaje.timestamp}`);
-            io.to(salaId).emit('receiveMsg', JSON.stringify(data));
             // Guardar el mensaje en la sesión correspondiente
             if (!memoryStorage.has(salaId)) {
                 memoryStorage.set(salaId, { datosCliente, datosAdmin, messages: [mensaje] });
 
-                // Unir al cliente al chat
-                socket.join(salaId);
-                // Realizar peticion al backend para actualizar el cliente admin y crear el chat en la base de datos
                 const adminUpdate = await mongoose.connection.collection('clientes').findOneAndUpdate(
                     { _id: new mongoose.Types.ObjectId(datosAdmin.idAdmin), 'chats._id': { $ne: salaId } },
                     {
@@ -57,11 +50,11 @@ module.exports = (serverNode) => {
                                     imagenCuenta: datosAdmin.imagenCuenta
                                 },
                                 mensajes: [
-                                    {
-                                        contenido: 'Bienvenido al soporte técnico, ¿necesitas ayuda?. Aquí podrás resolver tus dudas.',
-                                        timestamp: Date.now(),
-                                        transmitterId: datosAdmin.idAdmin.toString()
-                                    }
+                                    // {
+                                    //     contenido: 'Bienvenido al soporte técnico, ¿necesitas ayuda?. Aquí podrás resolver tus dudas.',
+                                    //     timestamp: Date.now(),
+                                    //     transmitterId: datosAdmin.idAdmin.toString()
+                                    // }
                                 ],
                                 fechaInicioChat: Date.now()
                             }
@@ -70,24 +63,44 @@ module.exports = (serverNode) => {
                     { returnDocument: "after" }
                 );
 
-                io.emit('adminJoinRoom', JSON.stringify({ keyChat: salaId, datosCliente, datosAdmin, firstMsg: mensaje }));
+                io.emit(`notification_admin_${datosAdmin.idAdmin}`, {
+                    salaId,
+                    datosCliente,
+                    datosAdmin,
+                    firstMsg: mensaje
+                });
+
+                await mongoose.connection.collection('clientes').updateMany(
+                    { 'chats._id': salaId },
+                    { $push: { "chats.$.mensajes": mensaje } }
+                );
+
+            } else {
+                memoryStorage.get(salaId).messages.push(mensaje);
+
+                await mongoose.connection.collection('clientes').updateMany(
+                    { 'chats._id': salaId },
+                    { $push: { "chats.$.mensajes": mensaje } }
+                );
             }
+
+            io.to(salaId).emit('receiveMsg', JSON.stringify(data));
         });
 
         // El administrador se une a la sala del cliente
-        socket.on('adminJoinRoom', (data) => {
-            const { keyChat, datosAdmin } = JSON.parse(data);
+        socket.on('adminJoinRoom', async (data) => {
+            const { keyChat, datosAdmin, datosCliente, firstMsg } = JSON.parse(data);
 
             if (memoryStorage.has(keyChat)) {
                 console.log('Admin uniendose a la sala');
                 socket.join(keyChat);
             }
+        })
 
-            // const session = memoryStorage.get(keyChat);
-            // if (session && session.messages.length > 0 && session.messages.length <= 1) {
-            //     //console.log('Enviando historial de mensajes al administrador para la sala: ', keyChat);
-            //     socket.emit('historyChat', JSON.stringify(session.messages));
-            // }
+        socket.on('joinRoom', async (data) => {
+            const { salaId } = JSON.parse(data);
+            // Unir al cliente normal a la sala
+            socket.join(salaId);
         })
     })
 }
